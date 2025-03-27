7b0dac80f6ed159605e85064f2be87ca4a2b5803
Linberg34 <pfqxbr2208@gmail.com>
2025-03-27 22:27:51 +0700

    Minor bug fixes with flyway

--- user-service/web/src/main/java/com/example/UserServiceWeb.java
+++ user-service/web/src/main/java/com/example/UserServiceWeb.java
@@ -4,11 +4,13 @@ import com.application.security.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example", "com.application"})
 @EnableConfigurationProperties(JwtProperties.class)
+@EnableMethodSecurity(prePostEnabled = true, jsr250Enabled = true)
 public class UserServiceWeb {
-    public  static  void main(String[] args){
+    public static void main(String[] args){
         SpringApplication.run(UserServiceWeb.class, args);
     }
 }
