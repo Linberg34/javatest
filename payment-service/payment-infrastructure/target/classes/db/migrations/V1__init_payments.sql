58eab9f05d2916664ad33b6262de2a1dd8ce7338
Linberg34 <pfqxbr2208@gmail.com>
2025-06-18 21:02:28 +0700

    initialized db

--- payment-service/payment-infrastructure/target/classes/db/migrations/V1__init_payments.sql
+++ payment-service/payment-infrastructure/target/classes/db/migrations/V1__init_payments.sql
@@ -1,5 +1,5 @@
 CREATE TABLE payments (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id UUID PRIMARY KEY,
     booking_id UUID NOT NULL,
     amount BIGINT NOT NULL,
     status VARCHAR(50) NOT NULL,
